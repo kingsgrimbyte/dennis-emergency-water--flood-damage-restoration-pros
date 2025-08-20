3f739aa0a280d6a61a23a466a3d650e2ef8d5bc7
kingsgrimbyte <kingsgrimbyte@gmail.com>
2025-08-20 06:43:53 +0000

    update robots.tx and sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 //  import CityData1 from "@/public/City.json";
 import contactContent from "@/app/Data/content";
 import subdomainContent from "@/app/Data/FinalContent";
@@ -6,7 +6,7 @@ import subdomainContent from "@/app/Data/FinalContent";
 const contentData: any = contactContent.contactContent;
 const data: any = subdomainContent.subdomainData;
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const SubDomain: any = Object.keys(data);
 
   const SubDomainURL = SubDomain.map((location: any) => ({
@@ -15,5 +15,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
     changeFrequency: "weekly",
     priority: 1,
   }));
-  return [...SubDomainURL];
+
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(createSitemap(SubDomainURL));
+}
+
+function createSitemap(urls: any[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+      .map((url) => {
+        return `
+      <url>
+        <loc>${url.url}</loc>
+        <lastmod>${url.lastModified.toISOString()}</lastmod>
+        <changefreq>${url.changeFrequency}</changefreq>
+        <priority>${url.priority}</priority>
+      </url>
+    `;
+      })
+      .join("")}
+  </urlset>`;
 }
